4730e011af831111f6a857c4702cee12647169b3
Kathy Chen <kathychen.social@gmail.com>
2019-05-21 19:56:35 -0400

    update index-spec.js

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,9 +1,6 @@
 // import { describe } from "mocha";
 
-import { describe } from "mocha";
-
-const chai = require('chai');
-const expect = chai.expect;
+const expect = require('chai').expect;
 const server = require('../server');
 
 console.log("test file, server = \n", server);
