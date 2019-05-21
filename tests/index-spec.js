80c6894bbb32495b5f90317578bc30a42eb61a81
Kathy Chen <kathychen.social@gmail.com>
2019-05-20 22:42:33 -0400

    added index-spec.js

---
+++ tests/index-spec.js
@@ -0,0 +1,16 @@
+// import { describe } from "mocha";
+
+import {describe} from "mocha";
+
+const chai = require('chai');
+const expect = chai.expect;
+const server = require('../server');
+
+console.log("test file, server = \n", server);
+
+describe('test', () => {
+    it('this simple test should return a string', () => {
+        expect('Test CI with Travis').to.equal('Test CI with Travis');
+    });
+    process.exit();
+});
