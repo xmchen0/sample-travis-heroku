9a90c33a9a776fc13cf415a9db45ca45f5cf87a6
Kathy Chen <kathychen.social@gmail.com>
2019-05-21 20:33:00 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-/* server.js main file */
+
+/* server.js main file 1.4 */
 const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001; // NOTE-TO-SELF: Travis uses 3000, do not use this port.
+
 app.get('/', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('req = \n', req);
-  res.send('<h1>Test CI with Travis</h1>');
+  res.send('Test CI with Travis 1.0');
 });
 
-const server = app.listen(3002, () => { // NOTE-TO-SELF: Travis uses 3000, do not use this port.
+const server = app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('app running on port 3002');
+  console.log('app running on port 3001');
 });
 
 // eslint-disable-next-line no-console
